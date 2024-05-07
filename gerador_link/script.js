50cd2d8d370ecb17d6aa07b7b0cd67e563259d15
Lauberr <130068637+Lauberr@users.noreply.github.com>
2024-05-07 19:57:25 -0300

    processos javaScript Gerador de link (incompleto)
    Criação de funções:  geraLink, abrirLink, valida.
    
    Reestruturando monitoramento de interações do usuário: clicks e inputs.
    
    Criação de mascara do numero de telefone.

--- gerador_link/script.js
+++ gerador_link/script.js
@@ -1,26 +1,41 @@
-const gerar = document.querySelectorAll("[divGerar]");
-const resultado = document.querySelectorAll("[divResultado]");
-const btnLink = document.querySelectorAll("[btn-link]");
-const btnAbrir = document.querySelectorAll("[btn-envia]");
-const inputLink = document.querySelectorAll("[input-link]").value;
-const inpuTel = document.querySelectorAll("[input-telefone]").value;
-
-// Botão de link
-btnLink.addeventListener("click", () => {
-    geraLink()
-    console.log
-})
-
-// Botão abrir em nova guia
-btnAbrir.addeventListener("click", () => {
-    abrirLink()
-})
+const resultado = document.querySelector("[divResultado]")
+const btnLink = document.querySelector("[btn-link]")
+const btnAbrir = document.querySelector("[btn-envia]")
+const link = document.querySelector("[input-link]")
+const telefone = document.querySelector("[input-telefone]")
+const desc = document.querySelector("[descricao]")
 
 /*=============================================================
-*                FUNÇÕES
+*                          INTERAÇÕES
 ==============================================================*/
 
-function geraLink(){
-    numero = inpuTel.innerText
-    inputLink.innerText = `https://wa.me/${numero}`
+btnLink.addEventListener("click", () => { geraLink() })
+btnAbrir.addEventListener("click", () => { abrirLink() })
+resultado.addEventListener("click", () => { copiar() })
+telefone.addEventListener("input", () => { valida() })
+
+/*=============================================================
+*                           FUNÇÕES
+==============================================================*/
+
+function geraLink(evento){
+    link.value = `https://wa.me/${telefone.value}`
+    resultado.style.display = "block"
+}
+
+function abrirLink(evento){
+    window.open(`https://wa.me/${telefone.value}`)
+}
+
+function valida(evento){
+    let tel = telefone.value;
+    tel = tel.replace(/\D/g, "");
+    if (tel.length > 11) {
+        tel = tel.slice(0, 11);
+    }
+    telefone.value = tel.replace(/(\d{2})(\d{5})(\d{4})/, "($1) $2-$3");
+}
+
+function copiar(evento){
+    
 }
