9950ceef2ceba807b93e8452e5785e7244bfff06
Lauberr <130068637+Lauberr@users.noreply.github.com>
2024-05-16 23:28:54 -0300

    contador de palavras e caracteres (COMPLETO)
    Projeto contador de palavras e caracteres
    COMPLETO

--- contador/script.js
+++ contador/script.js
@@ -1,23 +1,35 @@
-const inputTexto = document.querySelector("[input-texto]")
-const btnContar = document.querySelector("[btn-contar]")
-const divContador = document.querySelector("[txt-contador]")
+const inputTexto = document.querySelector("[input-texto]");
+const btnContar = document.querySelector("[btn-contar]");
+const divContador = document.querySelector("[txt-contador]");
 
-let tipoContagem = "caracteres"
+let tipoContagem = "caracteres";
 
 const handleInputTexto = (evento) => {
+    const texto = evento.target.value;
+    let contador;
 
+    if (tipoContagem === "caracteres") {
+        contador = texto.length;
+        divContador.innerText = `${contador} caracteres`;
 
-}
-
-function handleBtnContar(evento){
-    if(tipoContagem == "caracteres"){
-    evento.target.innerText = "Contar Palavras"
-    tipoContagem = "palavras"
     } else {
-    evento.target.innerText = "Contar Caracteres"
-    tipoContagem= "caracteres"
+        contador = texto.trim().split(/\s+/).filter(word => word !== "").length;
+        divContador.innerText = `${contador} palavras`;
     }
+};
+
+function handleBtnContar(evento) {
+    if (tipoContagem == "caracteres") {
+        evento.target.innerText = "Contar Caracteres";
+        tipoContagem = "palavras";
+
+    } else {
+        evento.target.innerText = "Contar Palavras";
+        tipoContagem = "caracteres";
+    }
+
+    handleInputTexto({ target: inputTexto });
 }
 
-btnContar.addEventListener("click", handleBtnContar)
-inputTexto.addEventListener("input", handleInputTexto)
+btnContar.addEventListener("click", handleBtnContar);
+inputTexto.addEventListener("input", handleInputTexto);
