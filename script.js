ebf1ce7b0c58f4066c45d64e076fc4318487a0c0
Lauberr <130068637+Lauberr@users.noreply.github.com>
2024-05-19 03:25:37 -0300

    inserindo nav entre as paginas

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-const proj = document.querySelectorAll("[box]")
+const calculadora = document.querySelector("[box1]")
+const contador = document.querySelector("[box2]")
+const gerador = document.querySelector("[box3]")
+
+calculadora.addEventListener("click", () => {
+    window.location.href = 'calculadora/index.html'
+})
+
+contador.addEventListener("click", () => {
+    window.location.href = 'contador/index.html'
+})
+
+gerador.addEventListener("click", () => {
+    window.location.href = 'gerador_link/index.html'
+})
